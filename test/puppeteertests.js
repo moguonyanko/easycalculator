110e287ff71f30902d7a000044caae2c72470330
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2018-12-04 18:17:56 +0900

    Added mastery calculation with skill

--- test/puppeteertests.js
+++ test/puppeteertests.js
@@ -17,6 +17,34 @@ const getPage = async ({browser, url}) => {
   return page;
 };
 
+// ElementHandleクラスにselectメソッドを実装しようとしているが実装できない。
+// Elementhandleがundefinedになっている。
+// 実装できれば以下のように呼び出せるはず。
+// 
+//        mainEle.select({
+//          root,
+//          selector: '.ship-selector',
+//          optionValue: '赤城改'
+//        });
+// 
+// 参考:
+// https://github.com/GoogleChrome/puppeteer/issues/613
+const extendElementHandle = () => {
+  if (typeof puppeteer.ElementHandle.prototype.select !== 'function') {
+    puppeteer.ElementHandle.prototype.select = async ({root = document,
+        selector, optionValue}) => {
+      await this._page.evaluateHandle((el, value) => {
+        const selectEle = root.querySelector(selector);
+        const optionEle = root.querySelector(`.${selector} option[value="${value}"]`);
+        optionEle.selected = true;
+        const event = new Event('change', {bubbles: true});
+        event.simulated = true;
+        selectEle.dispatchEvent(event);
+      }, this, optionValue);
+    };
+  }
+};
+
 /**
  * @private
  * @class
@@ -42,9 +70,12 @@ class UnitTestError extends Error {
 }
 
 /**
+ * @private
+ * @type Object
  * @name testCases
- * @description テストケース群です。アンダースコアで始まるテストケースはテスト実行に
- * 無視されます。
+ * @description テスト関数群です。
+ * $で始まるテスト関数は優先的に実行され他の関数は無視されます。
+ * _で始まるテスト関数は無視されます。
  */
 const testCases = {
   _testDummy( {browser, url}) {
@@ -135,7 +166,63 @@ const testCases = {
         acOption.selected = true;
         const event2 = new Event('change', {bubbles: true});
         event2.simulated = true;
-        acEle.dispatchEvent(event);
+        acEle.dispatchEvent(event2);
+
+        // 制空値計算実行
+        mainEle.querySelector('.calculator').click();
+        // ページに出力された制空値取得
+        return mainEle.querySelector('.result-area').innerText;
+      }, main);
+
+      if (actual === expected) {
+        resolve({name, actual});
+      } else {
+        reject(new UnitTestError({name, expected, actual}));
+      }
+    });
+  },
+  testCalculateMasteryWithMaxSkill( {browser, url}) {
+    return new Promise(async (resolve, reject) => {
+      const expected = '92';
+      const name = `赤城改の第3スロットに熟練度maxの烈風を積んだ時の制空値は ${expected} である`;
+
+      const page = await getPage({browser, url});
+      const main = await page.$('main');
+      const actual = await page.evaluate(mainEle => {
+        const shipName = '赤城改';
+        const slotNo = 3;
+        const acName = '烈風';
+
+        const root = mainEle.querySelector('.ships ship-config:first-of-type')
+            .shadowRoot;
+
+        // 艦艇リストから赤城改を選択するイベントを発生させる。
+        const shipSelector = '.ship-selector';
+        const shipEle = root.querySelector(shipSelector);
+        const shipOption = root.querySelector(`.ship-selector option[value="${shipName}"]`);
+        shipOption.selected = true;
+        const shipEvent = new Event('change', {bubbles: true});
+        shipEvent.simulated = true;
+        shipEle.dispatchEvent(shipEvent);
+
+        const targetSlotSelector = `.aircraft-selector-base .aircraft-selector-container:nth-of-type(${slotNo}) `;
+
+        // 赤城改の第3スロットのリストから烈風を選択するイベントを発生させる。
+        const acSelector = `${targetSlotSelector}.aircraft-selector`;
+        const acEle = root.querySelector(acSelector);
+        const acOption = root.querySelector(`${acSelector} option[value="${acName}"]`);
+        acOption.selected = true;
+        const acEvent = new Event('change', {bubbles: true});
+        acEvent.simulated = true;
+        acEle.dispatchEvent(acEvent);
+
+        // 烈風の熟練度をmaxにするイベントを発生させる。
+        const impSelector = `${targetSlotSelector}.aircraft-improve-range-container .aircraft-improve-range`;
+        const impEle = root.querySelector(impSelector);
+        impEle.value = '10';
+        const impEvent = new Event('change', {bubbles: true});
+        impEvent.simulated = true;
+        impEle.dispatchEvent(impEvent);
 
         // 制空値計算実行
         mainEle.querySelector('.calculator').click();
@@ -152,6 +239,30 @@ const testCases = {
   }
 };
 
+/**
+ * @private
+ * @function
+ * @name getTestCases
+ * @return {Array} テスト対象関数群 
+ * @description テスト対象の関数群を返します。
+ * 関数名が$で始まるテスト関数が見つかった場合はその関数だけを含む配列を返します。
+ * $で始まるテスト関数が複数存在した時は一番最初に見つかった関数だけを返します。
+ * _で始まるテスト関数は無視されます。
+ */
+const getTestCases = () => {
+  const priorityPrefix = "$";
+  const testCase = Object.values(testCases)
+      .find(testCase => testCase.name.startsWith(priorityPrefix));
+
+  if (testCase) {
+    return [testCase];
+  }
+
+  const ignorePrefix = '_';
+  return Object.values(testCases)
+      .filter(testCase => !testCase.name.startsWith(ignorePrefix));
+};
+
 const runAllTests = ({browser, url}) => {
   return new Promise((resolve, reject) => {
     if (!url) {
@@ -159,23 +270,19 @@ const runAllTests = ({browser, url}) => {
       return;
     }
 
-    const ignorePrefix = '_';
-
     // Promise.allによるテスト実行時にいずれかのテストが失敗しても他のテストが
     // 実行されるようPromise.catchを呼び出してreject時の振る舞いを変更している。
     // 参考: Promise.all fail-fast behaviour(MDN)
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
-    const tests = Object.values(testCases)
-        .filter(testCase => !testCase.name.startsWith(ignorePrefix))
-        .map(testCase => testCase({browser, url})
-              .then(result => {
-                console.log(`OK: ${result.name}`);
-                return result;
-              })
-              .catch(err => {
-                console.error(`NG: ${err.message}\n\t${err.diff()}`);
-                return err;
-              }));
+    const tests = getTestCases().map(testCase => testCase({browser, url})
+          .then(result => {
+            console.log(`OK: ${result.name}`);
+            return result;
+          })
+          .catch(err => {
+            console.error(`NG: ${err.message}\n\t${err.diff()}`);
+            return err;
+          }));
 
     Promise.all(tests).then(resolve);
   });
@@ -186,7 +293,7 @@ const main = async () => {
   const browser = await puppeteer.launch({
     // headlessはデフォルトでtrueである。ただしPage.evaluate内のコンソール出力の内容を
     // 確認したい時はheadlessをfalseにしてテストを実行する必要がある。
-    headless: true,
+    //headless: false,
     // 自己署名証明書に伴うエラーを無視するために必要。
     ignoreHTTPSErrors: true
   });
